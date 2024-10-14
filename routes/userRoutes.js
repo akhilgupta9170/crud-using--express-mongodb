a28d31517d58e3654f40de316e2704c2a4fc9fe7
Akhil gupta <akhilgupta18ald@gmail.com>
2024-10-14 11:14:01 +0530

    routes added

---
+++ routes/userRoutes.js
@@ -0,0 +1,13 @@
+    import express from 'express';
+    import { fetch ,create,update,deleteUser} from "../controller/userController.js";
+
+    const route = express.Router()
+
+    route.post("/create",create)
+
+    route.get('/fetch', fetch)
+
+    route.put("/update/:id", update);
+    route.delete("/delete", deleteUser);
+    export default route;
+
